c12803c9f036943546ac3960003039f8534957f4
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-30 16:32:21 +0900

    Create: lwrp site

--- resources/proxy.rb
+++
@@ -1,7 +0,0 @@
-actions :enable, :disable
-
-default_action :create
-
-attribute :upstream_name, :name_attribute => true
-attribute :backends, :requierd => true, :kind_of => Array
-attribute :servername, :kind_of => String, :default => "localhost"
