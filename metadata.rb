aaf42843e4b025f37df4c7be30308f42c35287dc
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-30 18:18:00 +0900

    bump 0.0.4

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures nginx_ppa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.4"
 depends          "apt"
