f6b5aeaf613aefc9a7c93ee1a7fdafa33ff24d6a
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-30 18:17:21 +0900

    Change notifire action reload to restart.

--- providers/site.rb
+++ providers/site.rb
@@ -2,7 +2,7 @@ action :enable do
   execute "nxensite #{new_resource.name}" do
     command "/usr/sbin/nxensite #{new_resource.name}"
     not_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}") end
-    notifies :restart, "service[nginx]"
+    notifies :reload, "service[nginx]"
     new_resource.updated_by_last_action(true)
   end
 end
@@ -11,7 +11,7 @@ action :disable do
   execute "nxdissite #{new_resource.name}" do
     command "/usr/sbin/nxdissite #{new_resource.name}"
     only_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}") end
-    notifies :restart, "service[nginx]"
+    notifies :reload, "service[nginx]"
     new_resource.updated_by_last_action(true)
   end
 end
